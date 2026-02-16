f3c46db6f6ef16e0dafd81984771aa419fca7d01
Rutuja2139 <wadgaonkarrutuja1@gmail.com>
2026-02-16 23:23:36 +0530

    Update UserService.java

--- src/main/java/com/demo/ecommerce/service/UserService.java
+++ src/main/java/com/demo/ecommerce/service/UserService.java
@@ -9,24 +9,20 @@ import java.util.List;
 public class UserService {
     private final UserRepository repo;
 
-    public UserService(UserRepository repo) { this.repo = repo; }
+    public UserService(UserRepository repo) { 
+        this.repo = repo; 
+    }
 
     public User register(User user) {
-
         List<User> existingUsers = repo.findAllByEmail(user.getEmail());
-
-
         if (!existingUsers.isEmpty()) {
             return null;
         }
-
-
         return repo.save(user);
     }
 
     public User login(String email, String password) {
         List<User> users = repo.findAllByEmail(email);
-
         for (User user : users) {
             if (user.getPassword() != null && user.getPassword().equals(password)) {
                 return user;
