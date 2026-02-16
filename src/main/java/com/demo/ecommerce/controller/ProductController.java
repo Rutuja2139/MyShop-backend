9274eaa49fcd467b77e8e720d01f9d428966a2ef
Rutuja2139 <wadgaonkarrutuja1@gmail.com>
2026-02-16 22:57:09 +0530

    Update ProductController.java

--- src/main/java/com/demo/ecommerce/controller/ProductController.java
+++ src/main/java/com/demo/ecommerce/controller/ProductController.java
@@ -7,21 +7,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/products")
-
 @CrossOrigin(origins = "${app.frontend.url}", allowCredentials = "true")
 public class ProductController {
     private final ProductService service;
 
-    public ProductController(ProductService service) { this.service = service; }
+    public ProductController(ProductService service) { 
+        this.service = service; 
+    }
 
     @PostMapping
     public Product addProduct(@RequestBody Product product) {
-
         return service.addProduct(product);
     }
 
     @GetMapping
     public List<Product> getAll() {
         return service.getAllProducts();
+    }
+
+
+    
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        service.deleteProduct(id);
     }
 }
